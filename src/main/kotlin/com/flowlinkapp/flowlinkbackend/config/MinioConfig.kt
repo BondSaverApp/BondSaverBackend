e0581b99fa75675e2aa56b87cfdf03073c1aaf7e
uiop07558 <uiop07558@gmail.com>
2025-05-27 18:37:18 +0300

    bug fix

--- src/main/kotlin/com/flowlinkapp/flowlinkbackend/config/MinioConfig.kt
+++ src/main/kotlin/com/flowlinkapp/flowlinkbackend/config/MinioConfig.kt
@@ -3,7 +3,9 @@ package com.flowlinkapp.flowlinkbackend.config
 import io.minio.MinioClient
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 
+@Configuration
 class MinioConfig(
   @Value("\${s3.host}")
   val host: String,
